2132ac944a69a386573f0f871fd0c09521f9e379
Andras Novoszath <nocibambi@gmail.com>
2023-04-19 21:25:02 +0200

    Unit tests

--- test_imdb_quest.py
+++ test_imdb_quest.py
@@ -20,3 +20,21 @@ def top_movies() -> pd.DataFrame:
     )
 
 
+def test_scraper():
+    # The `scraper` function validates the data in running time.
+    # Breaking up the function would allow more granular testing. TBD.
+    movies = scraper(top_number=3)
+    assert not movies.isna().any().any()
+
+
+def test_review_penalizer(top_movies):
+    assert all(
+        adjust_by_number_of_rankings(top_movies).values
+        == pd.Series([-0, -1.1, -2]).values
+    )
+
+
+def test_oscar_calculator(top_movies):
+    assert all(
+        adjust_by_number_of_oscars(top_movies).values == pd.Series([0, 0.3, 0.5]).values
+    )
